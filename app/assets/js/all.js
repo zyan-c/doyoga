49198b771652a218493992457b0081e260e32a12
zyan-c <zyanchang950@gmail.com>
2021-08-25 15:10:42 +0800

    revise

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,20 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 4.3,
+      spaceBetween: 20,
+    },
+  },
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
