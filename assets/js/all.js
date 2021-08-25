101d48684164e0453dab10f67e0bc729b9febafb
zyan-c <zyanchang950@gmail.com>
2021-08-25 15:10:57 +0800

    Update 2021-08-25T07:10:56.304Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,23 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1.5,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 4.3,
+      spaceBetween: 20
+    }
+  },
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 //# sourceMappingURL=all.js.map
